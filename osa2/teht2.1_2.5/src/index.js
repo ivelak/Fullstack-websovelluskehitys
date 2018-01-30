85644a72a957cd0a413d8d43545f1288a0e81ebc
ivelak <rantaana@gmail.com>
2018-01-30 21:21:29 +0200

    tehtava 2.1

---
+++ osa2/teht2.1_2.5/src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
+
+/*
+const Yhteensa = (props) => {
+  const [osa1, osa2, osa3] = props.kurssi.osat
+
+  return (
+    <p>yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
+  )
+}
+*/
+const Kurssi = ({ kurssi }) => {
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <ul>
+        {kurssi.osat.map(osa => <li key={osa.id}>{osa.nimi} {osa.tehtavia}</li>)}
+      </ul>
+    </div>
+  )
+
+}
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10,
+        id: 1
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7,
+        id: 2
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14,
+        id: 3
+      },
+      {
+        nimi: 'Lisätään vielä yksi',
+        tehtavia: 14,
+        id: 4
+      }
+    ]
+  }
+  return (
+    <div>
+      <Kurssi kurssi={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
