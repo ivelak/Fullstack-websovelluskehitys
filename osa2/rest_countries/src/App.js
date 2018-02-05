3df9aab8a43062149a3c7879fe95a68ca2f99a44
ivelak <rantaana@gmail.com>
2018-02-05 15:41:51 +0200

    tehtava 2.12

--- osa2/rest_countries/src/App.js
+++ osa2/rest_countries/src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import countryService from './services/countries';
 
+const Switch = ({ cunts }) => {
+  if (cunts.length === 1) {
+    const cunt = cunts[0]
+    console.log('yksi vain')
+    return (
+
+      <div>
+        <h2>{cunt.name}</h2>
+        <p>capital: {cunt.capital}</p>
+        <p>population: {cunt.population}</p>
+        <img src={cunt.flag} width="200" />
+      </div>
+    )
+  }
+  if (cunts.length < 10) {
+    console.log('alle 10')
+    return (
+      <ul>
+        {cunts.map(country => <li key={country.name}>  {country.name} </li>)}
+      </ul>
+    )
+  } else {
+    console.log('liikaa', cunts.length)
+    return (
+
+      <div>
+
+        <p>Tarkenna rajausta</p>
+      </div>
+    )
+  }
+
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       countries: [],
-      filter: '',
-      show: false
+      filter: ''
     }
   }
   componentWillMount() {
@@ -24,14 +57,9 @@ class App extends React.Component {
     console.log(event.target.value, filtered.map(country => country.name))
   }
   filteredCountries = () => {
-    if (this.state.filter.length === 0) return []
+
     const filtered = this.state.countries.filter(country => country.name.toUpperCase().includes(this.state.filter.toUpperCase()))
-    if (filtered.length<10){
-      this.setState ({
-        show: true
-      })
-    }
-    return 
+    return filtered
   }
 
   render() {
@@ -43,10 +71,9 @@ class App extends React.Component {
             onChange={this.handleFilterChange}
           />
         </div>
-
-        <ul>
-          {this.filteredCountries().map(country => <li key={country.name}>  {country.name} </li>)}
-        </ul>
+        <div>
+          <Switch cunts={this.filteredCountries()} />
+        </div>
 
       </div>
     )
