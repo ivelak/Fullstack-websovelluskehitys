a8eaef6eb0efee583946f6f70c5cb79b00aeb328
ivelak <rantaana@gmail.com>
2018-03-04 23:30:19 +0200

    5.20

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -3,25 +3,28 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    const anecdotes = this.props.store.getState()
+    let anecdotes = this.props.store.getState()
+    anecdotes = anecdotes.sort(function (a, b) {
+      return b.votes - a.votes
+    })
     return (
       <div>
         <h2>Anecdotes</h2>
-        {anecdotes.map(anecdote=>
+        {anecdotes.map(anecdote =>
           <div key={anecdote.id}>
             <div>
-              {anecdote.content} 
+              {anecdote.content}
             </div>
             <div>
               has {anecdote.votes}
-              <button onClick={()=>this.props.store.dispatch({type: 'VOTE', data: {id: anecdote.id}})}>vote</button>
+              <button onClick={() => this.props.store.dispatch({ type: 'VOTE', data: { id: anecdote.id } })}>vote</button>
             </div>
           </div>
         )}
         <h2>create new</h2>
         <form>
           <div><input /></div>
-          <button>create</button> 
+          <button>create</button>
         </form>
       </div>
     )
