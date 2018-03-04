6c4ef9b218f2e6d8e72c79ce8bfbf01dd944f540
ivelak <rantaana@gmail.com>
2018-03-04 22:59:17 +0200

    5.18

--- redux_unicafe/src/index.js
+++ redux_unicafe/src/index.js
@@ -1,67 +1,87 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import counterReducer from './reducer'
+
+const store = createStore(counterReducer)
 
 const Statistiikka = () => {
-  const palautteita = 0
+    const palautteita = store.getState().good + store.getState().ok + store.getState().bad
+    console.log(palautteita)
 
-  if (palautteita === 0) {
+    const avg = () => {
+        return (store.getState().good - store.getState().bad) / palautteita
+    }
+    const posit = () => {
+        return (store.getState().good / palautteita * 100) + '%'
+    }
+
+
+    if (palautteita === 0) {
+        return (
+            <div>
+                <h2>stataistiikka</h2>
+                <div>ei yhtään palautetta annettu</div>
+            </div>
+        )
+    }
+
     return (
-      <div>
-        <h2>stataistiikka</h2>
-        <div>ei yhtään palautetta annettu</div>
-      </div>
+        <div>
+            <h2>statistiikka</h2>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>hyvä</td>
+                        <td>{store.getState().good}</td>
+                    </tr>
+                    <tr>
+                        <td>neutraali</td>
+                        <td>{store.getState().ok}</td>
+                    </tr>
+                    <tr>
+                        <td>huono</td>
+                        <td>{store.getState().bad}</td>
+                    </tr>
+                    <tr>
+                        <td>keskiarvo</td>
+                        <td>{avg()}</td>
+                    </tr>
+                    <tr>
+                        <td>positiivisia</td>
+                        <td>{posit()}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <button onClick={()=>store.dispatch({ type: 'ZERO' })}>nollaa tilasto</button>
+        </div >
     )
-  }
-
-  return (
-    <div>
-      <h2>statistiikka</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td>hyvä</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>neutraali</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>huono</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>keskiarvo</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>positiivisia</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-
-      <button>nollaa tilasto</button>
-    </div >
-  )
 }
 
 class App extends React.Component {
-  klik = (nappi) => () => {
+    klik = (nappi) => () => {
+        store.dispatch({ type: nappi })
 
-  }
+    }
 
-  render() {
-    return (
-      <div>
-        <h2>anna palautetta</h2>
-        <button onClick={this.klik('GOOD')}>hyvä</button>
-        <button onClick={this.klik('OK')}>neutraali</button>
-        <button onClick={this.klik('BAD')}>huono</button>
-        <Statistiikka />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h2>anna palautetta</h2>
+                <button onClick={this.klik('GOOD')}>hyvä</button>
+                <button onClick={this.klik('OK')}>neutraali</button>
+                <button onClick={this.klik('BAD')}>huono</button>
+                <Statistiikka />
+            </div>
+        )
+    }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+    ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+render()
+store.subscribe(render)
+
