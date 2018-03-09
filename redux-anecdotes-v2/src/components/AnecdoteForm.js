aea55104d46478477178bb8d197fef9ce3ec997e
ivelak <rantaana@gmail.com>
2018-03-09 22:35:35 +0200

    6.1 ja 6.2

---
+++ redux-anecdotes-v2/src/components/AnecdoteForm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { anecdoteCreation } from './../reducers/anecdoteReducer'
+
+
+class AnecdoteForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.store.dispatch(
+      anecdoteCreation(e.target.anecdote.value)
+    )
+    e.target.anecdote.value = ''
+  }
+  render() {
+    return (
+      <div>
+        <h2>create new</h2>
+        <form onSubmit={this.handleSubmit}>
+          <div><input name='anecdote' /></div>
+          <button>create</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default AnecdoteForm
